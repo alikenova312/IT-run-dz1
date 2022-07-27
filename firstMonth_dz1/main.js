9331597ae507973938460888e5b3fc32504a68d5
alikenova312 <alikenova312@icloud.com>
2022-07-27 22:07:07 +0600

    dz1

---
+++ firstMonth_dz1/main.js
@@ -0,0 +1,58 @@
+// let num = 5;
+// console.log(num);
+
+// let a = 10;
+// let b = 2;
+// let c = 5;
+// console.log(a + b + c);
+
+// let a = 100;
+// let b = 2;
+// console.log(a * b);
+
+// let a = 67;
+// let b = 7;
+// console.log(a / b);
+
+// let a = 10;
+// let b = 2;
+// console.log(a + b);
+// console.log(a - b);
+// console.log(a * b);
+// console.log(a / b);
+
+// let c = 15;
+// let d = 2;
+//
+// let result = c + d;
+// console.log(result)
+
+// let a = 30;
+// let b = 15;
+// console.log(a + b);
+//
+// let c = 45;
+// let d = 4;
+// console.log(c / d);
+
+// let a = 17;
+// let b = 10;
+// console.log(a - b);
+
+// let c = 7;
+// let d = 8;
+//
+// let result = c + d;
+// console.log(result);
+
+// let a = '10';
+// let b = '99';
+//
+// let result = a + b;
+// console.log(result);
+
+// let name = 'Asi';
+// let num = 10;
+//
+// let result = name + num;
+// console.log(result);
